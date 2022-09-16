9b0d6138ec9721d37d6171ae438ebbe47a5bb3a7
WiseJade <jeonhr861@gmail.com>
2022-09-16 12:22:48 +0900

    chore: spring boot 초기 셋팅
    배포 DB : MariaDB
    테스트 DB : h2
    REST API Docs : swagger
    others : jwt, Oauth2, security, mapstruct, validation

---
+++ server/catvillage/src/main/java/com/twentyfour_seven/catvillage/CatvillageApplication.java
@@ -0,0 +1,13 @@
+package com.twentyfour_seven.catvillage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatvillageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatvillageApplication.class, args);
+	}
+
+}
---
+++ server/catvillage/src/test/java/com/twentyfour_seven/catvillage/CatvillageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twentyfour_seven.catvillage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatvillageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
