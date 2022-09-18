7136be96b454e352f3a193c3e679093e0ded65a1
leejuhanKr <96106122+leejuhanKr@users.noreply.github.com>
2022-09-18 16:47:50 +0900

    chore: 기본적인 폴더 구조 생성

---
+++ client/src/pages/Page404/index.tsx
@@ -0,0 +1,9 @@
+const Page404 = () => {
+  return (
+    <div>
+      404 페이지입니다.
+    </div>
+  )
+}
+
+export default Page404
---
+++ client/src/pages/Page500/index.tsx
@@ -0,0 +1,9 @@
+const Page500 = () => {
+  return (
+    <div>
+      500 페이지입니다.
+    </div>
+  )
+}
+
+export default Page500
