df5fcda719a3a46d063e52b14aaf8cccf730a30b
ddaisylee <elliejay9988@gmail.com>
2022-09-19 14:14:17 +0900

    style: GlobalStyle 적용
    styled-components 설치 및 GolbalStyle 적용

---
+++ client/src/styles/GlobalStyle.ts
@@ -0,0 +1,37 @@
+import { createGlobalStyle } from 'styled-components'
+import reset from 'styled-reset'
+
+export const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+  * {
+    box-sizing: border-box;
+  }
+
+  html, body {
+    font-family: -apple-system,BlinkMacSystemFont,"Segoe UI Adjusted","Segoe UI","Liberation Sans",sans-serif;
+  }
+  
+  input, textarea { 
+    -moz-user-select: auto;
+    -webkit-user-select: auto;
+    -ms-user-select: auto;
+    user-select: auto;
+  }
+  
+  input:focus {
+    outline: none;
+  }
+  
+  button {
+    border: none;
+    background: none;
+    padding: 0;
+    cursor: pointer;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+`
