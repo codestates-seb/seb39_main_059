5de5502173360d4ee0fcf7ea287383f56a789c94
ijyu <ijyu93@gmail.com>
2022-09-19 15:23:35 +0900

    feat: custom bean utils 추가
    resolved #32
    patch 동작에 사용시 편리한 유틸 클래스 추가

---
+++ server/catvillage/src/main/java/com/twentyfour_seven/catvillage/utils/CustomBeanUtils.java
@@ -0,0 +1,30 @@
+package com.twentyfour_seven.catvillage.utils;
+
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Field;
+import java.util.Collection;
+
+@Component
+public class CustomBeanUtils<T> {
+    public T copyNonNullProperties(T source, T destination) {
+        if(source == null || destination == null || source.getClass() != destination.getClass()) {
+            return null;
+        }
+
+        final BeanWrapper src = new BeanWrapperImpl(source);
+        final BeanWrapper dest = new BeanWrapperImpl(destination);
+
+        for(final Field property : source.getClass().getDeclaredFields()) {
+            Object sourceProperty = src.getPropertyValue(property.getName());
+            Object destinationProperty = dest.getPropertyValue(property.getName());
+
+            if(sourceProperty != null && !sourceProperty.equals(destinationProperty) && !(sourceProperty instanceof Collection<?>)) {
+                dest.setPropertyValue(property.getName(), sourceProperty);
+            }
+        }
+        return destination;
+    }
+}
