dc42fcac42b22f6a6411daaad49338e5938e8bda
ddaisylee <elliejay9988@gmail.com>
2022-09-21 00:25:02 +0900

    fix: 공통 레이아웃 수정
    height 설정으로 인한 border 잘림 현상 해결
    
    텍스트 넘침 현상 해결

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import { ThemeProvider } from 'styled-components'
 import { GlobalStyle } from './styles/GlobalStyle'
 import theme from './styles/theme'
+import { Iphone11ProContainer } from './styles/Iphone11ProContainer'
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+      <Iphone11ProContainer>{/* 하위 컴포넌트 */}</Iphone11ProContainer>
     </ThemeProvider>
   )
 }
--- client/src/styles/Iphone11ProContainer.tsx
+++ client/src/styles/Iphone11ProContainer.tsx
@@ -4,4 +4,9 @@ export const Iphone11ProContainer = styled.div`
   border: 3px solid black;
   width: 375px;
   height: 812px;
+  overflow: scroll;
+  -ms-overflow-style: none;
+  &::-webkit-scrollbar {
+    display: none;
+  }
 `
