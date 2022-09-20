3382a3a59b9436a83abf6bff020a4299560e2418
WiseJade <jeonhr861@gmail.com>
2022-09-20 11:11:16 +0900

    feat: 특정 고양이 프로필 보기 기능 구현
    resolve #10
    CatController, CatService, CatMapper에 get-cat에 필요한 로직 구현

--- server/catvillage/src/main/java/com/twentyfour_seven/catvillage/cat/mapper/CatMapper.java
+++ server/catvillage/src/main/java/com/twentyfour_seven/catvillage/cat/mapper/CatMapper.java
@@ -1,12 +1,13 @@
 package com.twentyfour_seven.catvillage.cat.mapper;
 
 import com.twentyfour_seven.catvillage.cat.dto.CatPostDto;
+import com.twentyfour_seven.catvillage.cat.dto.CatResponseDto;
 import com.twentyfour_seven.catvillage.cat.entity.Cat;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 
 import java.time.LocalDateTime;
-import java.util.Date;
+import java.util.ArrayList;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface CatMapper {
@@ -23,5 +24,26 @@ public interface CatMapper {
                 .body(catPostDto.getBody())
                 .build();
         return cat;
+    }
+
+    default public CatResponseDto catToCatResponseDto(Cat cat) {
+        if (cat == null) {
+            return null;
+        }
+        LocalDateTime birthDate = cat.getBirthDate();
+        CatResponseDto catResponseDto = new CatResponseDto(
+                cat.getCatId(),
+                cat.getName(),
+                birthDate.getYear(),
+                birthDate.getMonthValue(),
+                birthDate.getDayOfMonth(),
+                cat.getBreed().getKorName(),
+                cat.getSex(),
+                cat.getWeight(),
+                cat.getImage(),
+                cat.getBody(),
+                new ArrayList<>()
+        );
+        return catResponseDto;
     }
 }
