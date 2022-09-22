0e43d1d77c6236323b8b67a7615ed2f59a6ab0f7
leejuhanKr <96106122+leejuhanKr@users.noreply.github.com>
2022-09-22 19:11:15 +0900

    refactor: 파일 구조 변경

---
+++ client/src/components/atoms/Comment/Comment.style.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+export const Commentlayout = styled.li`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 7px;
+  margin-top: 7px;
+`
