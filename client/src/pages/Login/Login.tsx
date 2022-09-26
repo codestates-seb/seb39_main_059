45773e270b55252eaa6b25bcf6cedc51198c95d8
ddaisylee <elliejay9988@gmail.com>
2022-09-26 23:49:59 +0900

    feat: 로그인 페이지 마크업

---
+++ client/src/pages/Login/Login.tsx
@@ -0,0 +1,73 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import { ChangeEvent, useState } from 'react'
+import SocialLoginButton from '@Atoms/SocialLoginButton'
+import * as S from './Login.style'
+
+const Login = () => {
+  const [id, setId] = useState('')
+  const [password, setPassword] = useState('')
+  const [isShown, setIsSHown] = useState(false)
+
+  const togglePassword = () => {
+    setIsSHown(isShown => !isShown)
+  }
+
+  const handleIdChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setId(e.target.value)
+  }
+
+  const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value)
+  }
+
+  const handleLogin = () => {
+    /* 일반 로그인 로직 */
+  }
+
+  const handleSocialLogin = () => {
+    /* 소셜 로그인 로직 */
+  }
+
+  return (
+    <S.LoginLayout>
+      <S.Logo />
+      <S.Description>여러분의 고양이를 자랑해보세요</S.Description>
+      <S.InputBox>
+        <S.LoginInput
+          type="text"
+          inputName="id"
+          value={id}
+          placeholder="아이디"
+          onChange={handleIdChange}
+        />
+        <S.LoginInput
+          type={isShown ? 'text' : 'password'}
+          inputName="password"
+          value={password}
+          placeholder="비밀번호"
+          onChange={handlePasswordChange}
+        />
+        <S.Checkbox>
+          <input type="checkbox" checked={isShown} onChange={togglePassword} />
+          <label htmlFor="checkbox">비밀번호 보기</label>
+        </S.Checkbox>
+      </S.InputBox>
+      <S.LoginButton
+        backgroundColor="primary"
+        fontSize="lg"
+        fontWeight="bold"
+        color="white"
+        onClick={handleLogin}
+      >
+        로그인
+      </S.LoginButton>
+      <S.TextBox>
+        <S.FindText>아이디/비밀번호 찾기</S.FindText>
+        <S.SignupText>회원가입</S.SignupText>
+      </S.TextBox>
+      <SocialLoginButton onClick={handleSocialLogin} />
+    </S.LoginLayout>
+  )
+}
+
+export default Login
