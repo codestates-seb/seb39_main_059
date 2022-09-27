5f2d99a62df282076a387c7f3d74b1ae224a77b6
WiseJade <jeonhr861@gmail.com>
2022-09-27 16:13:02 +0900

    fix: FeedResponseDto 수정
    FeedResponseDto에 String 타입 List로 필드 값 가지고 있던 것을 각각 dto 리스트로 수정함
    feedTag를 제외한 모든 필드값을 가지는 생성자 생성

--- server/catvillage/src/main/java/com/twentyfour_seven/catvillage/feed/dto/FeedResponseDto.java
+++ server/catvillage/src/main/java/com/twentyfour_seven/catvillage/feed/dto/FeedResponseDto.java
@@ -1,5 +1,6 @@
 package com.twentyfour_seven.catvillage.feed.dto;
 
+import com.twentyfour_seven.catvillage.common.picture.dto.PictureDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,7 +15,14 @@ import java.util.List;
 public class FeedResponseDto {
     private long feedId;
     private long catId;
-    private List<String> picture;
+    private List<PictureDto> pictures;
     private String body;
-    private List<String> tag;
+    private List<FeedTagDto> tags;
+
+    public FeedResponseDto(long feedId, long catId, List<PictureDto> pictures, String body) {
+        this.feedId = feedId;
+        this.catId = catId;
+        this.pictures = pictures;
+        this.body = body;
+    }
 }
