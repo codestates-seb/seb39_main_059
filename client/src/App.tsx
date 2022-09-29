65e54a0ba377481a398eb0b871519b2ed3a820b5
leejuhanKr <96106122+leejuhanKr@users.noreply.github.com>
2022-09-30 07:45:54 +0900

    feat: 라우터 기능 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,22 @@
+import { Suspense } from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 import { GlobalStyle } from '@Styles/GlobalStyle'
 import theme from '@Styles/theme'
-import { Iphone11ProContainer } from '@Styles/Iphone11ProContainer'
+import AppRoutes from './routes'
+
+const loading = <div>화면을 불러오는 중 입니다.</div>
 
 const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Iphone11ProContainer>{/* 하위 컴포넌트 */}</Iphone11ProContainer>
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Suspense fallback={loading}>
+          <AppRoutes />
+        </Suspense>
+      </ThemeProvider>
+    </BrowserRouter>
   )
 }
 
