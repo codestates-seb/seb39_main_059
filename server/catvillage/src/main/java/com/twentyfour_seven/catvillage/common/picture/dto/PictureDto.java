0fece438f0c4379d2679ffff3d7476ee5e49b186
WiseJade <jeonhr861@gmail.com>
2022-09-30 18:07:09 +0900

    fix: postDto에서 유효성 검증에 빠진 코드 추가

--- server/catvillage/src/main/java/com/twentyfour_seven/catvillage/common/picture/dto/PictureDto.java
+++ server/catvillage/src/main/java/com/twentyfour_seven/catvillage/common/picture/dto/PictureDto.java
@@ -5,12 +5,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
+import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.Max;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@Validated
 public class PictureDto {
     @Length(max = 500)
     private String picture;
--- server/catvillage/src/main/java/com/twentyfour_seven/catvillage/feed/dto/FeedTagDto.java
+++ server/catvillage/src/main/java/com/twentyfour_seven/catvillage/feed/dto/FeedTagDto.java
@@ -4,11 +4,15 @@ import com.twentyfour_seven.catvillage.feed.entity.FeedTag;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.validation.annotation.Validated;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@Validated
 public class FeedTagDto {
+    @Length(max = 15)
     private String tag;
 
     public FeedTagDto (FeedTag feedTag) {
