6070d3db0806788a41040348a11428dd35f2a0dc
WiseJade <jeonhr861@gmail.com>
2022-09-30 16:11:30 +0900

    feat: 냥이생활 전체 피드보기 기능 구현
    응답 dto 안에 전체 피드 정보dto 와 팔로우 정보를 담은 dto 포함
    like 엔티티 구현 전이므로 like 여부 확인하는 로직은 차후에 추가 필요합니다!!
    로그인 정보가 있다면(UserDetails) 팔로우 목록 꺼내서 FollowFeedGetDto에 담음

---
+++ server/catvillage/src/main/java/com/twentyfour_seven/catvillage/feed/dto/FeedMultiGetResponseDto.java
@@ -0,0 +1,25 @@
+package com.twentyfour_seven.catvillage.feed.dto;
+
+import io.swagger.annotations.ApiParam;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class FeedMultiGetResponseDto {
+    @ApiParam(value = "냥이생활 피드 식별자(id)")
+    private long feedId;
+    @ApiParam(value = "특정 피드의 대표이미지")
+    private String image;
+    @ApiParam(value = "로그인한 사용자가 좋아요를 눌렀는지 여부")
+    private boolean isLike;
+
+    public FeedMultiGetResponseDto(long feedId, String image) {
+        this.feedId = feedId;
+        this.image = image;
+    }
+}
