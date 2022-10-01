21f68d1ca2fab16adf55dc001ea09d5dda34647b
WiseJade <jeonhr861@gmail.com>
2022-10-01 15:41:18 +0900

    feat: patch-feed 기능 구현
    FeedController에 patchFeed 메서드 구현
    FeedService에서 pictures, feed의 body 업데이트 하는 updateFeed 메서드 구현
    FeedTagService에 tag 정보 업데이트하는 updateTags 구현

--- server/catvillage/src/main/java/com/twentyfour_seven/catvillage/feed/service/FeedService.java
+++ server/catvillage/src/main/java/com/twentyfour_seven/catvillage/feed/service/FeedService.java
@@ -2,6 +2,7 @@ package com.twentyfour_seven.catvillage.feed.service;
 
 import com.twentyfour_seven.catvillage.cat.entity.Cat;
 import com.twentyfour_seven.catvillage.cat.service.CatService;
+import com.twentyfour_seven.catvillage.common.picture.entity.Picture;
 import com.twentyfour_seven.catvillage.common.picture.service.PictureService;
 import com.twentyfour_seven.catvillage.exception.BusinessLogicException;
 import com.twentyfour_seven.catvillage.exception.ExceptionCode;
@@ -11,6 +12,7 @@ import com.twentyfour_seven.catvillage.user.controller.FollowService;
 import com.twentyfour_seven.catvillage.user.entity.User;
 import com.twentyfour_seven.catvillage.user.repository.UserRepository;
 import com.twentyfour_seven.catvillage.user.service.UserService;
+import com.twentyfour_seven.catvillage.utils.CustomBeanUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
@@ -78,5 +80,55 @@ public class FeedService {
                 }
         );
         return followCats;
+    }
+
+    public Feed updateFeed(Feed feed, String email) {
+        // feedId로 저장되있던 feed 정보를 불러옴
+        Feed findFeed = findVerifiedFeed(feed.getFeedId());
+
+        // 글을 처음 작성한 유저와 로그인한 유저가 동일한지 확인하고 아니면 Exception throw
+        if (!findFeed.getCat().getUser().getEmail().equals(email)) {
+            throw new BusinessLogicException(ExceptionCode.INVALID_USER);
+        }
+
+        // Feed 내용 업데이트
+        Feed updateFeed = findFeed;
+        if (findFeed.getBody() != feed.getBody()) {
+            updateFeed.setBody(feed.getBody());
+        }
+
+        // Picture 업데이트를 위해 source, definition의 path만 따로 저장
+        List<String> picturePaths1 = new ArrayList<>();
+        List<String> picturePaths2 = new ArrayList<>();
+        feed.getPictures().forEach(
+                picture -> {
+                    picturePaths1.add(picture.getPath());
+                }
+        );
+        updateFeed.getPictures().forEach(
+                picture -> {
+                    picturePaths2.add(picture.getPath());
+                }
+        );
+        // 삭제된 이미지는 DB에서 제거 후 리스트에서도 제거
+        findFeed.getPictures().forEach(
+                picture -> {
+                    if (!picturePaths1.contains(picture.getPath())) {
+                        pictureService.removePicture(picture.getPictureId());
+                        findFeed.getPictures().remove(picture);
+                    }
+                }
+        );
+
+        // 추가된 이미지는 DB에 추가 후 리스트에도 추가
+        feed.getPictures().forEach(
+                picture -> {
+                    if (!picturePaths2.contains(picture.getPath())) {
+                        picture = pictureService.createPicture(picture);
+                        findFeed.getPictures().add(picture);
+                    }
+                }
+        );
+        return feedRepository.save(updateFeed);
     }
 }
