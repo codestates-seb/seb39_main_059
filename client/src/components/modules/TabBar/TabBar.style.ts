d074497546bbc1c62d45d5f8d3f64a215e25018a
ddaisylee <elliejay9988@gmail.com>
2022-10-02 15:52:04 +0900

    style: TabBar 컴포넌트 위치 고정

--- client/src/components/modules/TabBar/TabBar.style.ts
+++ client/src/components/modules/TabBar/TabBar.style.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-export const TabBarLayout = styled.div`
+export const TabBarLayout = styled.nav`
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -10,7 +10,8 @@ export const TabBarLayout = styled.div`
   box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.25);
   border-radius: 43px;
   position: absolute;
-  top: 100px;
+  left: calc(50% - 230px / 2);
+  bottom: 250px;
 `
 export const IconBox = styled.div`
   display: flex;
