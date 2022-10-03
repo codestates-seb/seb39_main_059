7d2c704ed9ecdf66a91c9a6e0805d04322f67aeb
WiseJade <jeonhr861@gmail.com>
2022-10-04 01:23:47 +0900

    feat: 고양이 품종정보 등록 기능 구현

--- server/catvillage/src/main/java/com/twentyfour_seven/catvillage/catInfo/service/CatInfoService.java
+++ server/catvillage/src/main/java/com/twentyfour_seven/catvillage/catInfo/service/CatInfoService.java
@@ -1,7 +1,31 @@
 package com.twentyfour_seven.catvillage.catInfo.service;
 
+import com.twentyfour_seven.catvillage.catInfo.entity.CatInfo;
+import com.twentyfour_seven.catvillage.catInfo.repository.CatInfoRepository;
+import com.twentyfour_seven.catvillage.exception.BusinessLogicException;
+import com.twentyfour_seven.catvillage.exception.ExceptionCode;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CatInfoService {
+
+    private final CatInfoRepository catInfoRepository;
+
+    public CatInfoService(CatInfoRepository catInfoRepository) {
+        this.catInfoRepository = catInfoRepository;
+    }
+
+    public CatInfo createCatInfo(CatInfo catInfo) {
+        verifiedExistKorName(catInfo.getKorName());
+        return catInfoRepository.save(catInfo);
+    }
+
+    private void verifiedExistKorName(String korName) {
+        catInfoRepository.findByKorName(korName)
+                .ifPresent(a -> {
+                    throw new BusinessLogicException(ExceptionCode.BREED_EXISTS);
+                });
+    }
 }
