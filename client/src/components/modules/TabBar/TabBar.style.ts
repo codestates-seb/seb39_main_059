328fa11a13c92bf5a420831e62899c09bbbacf98
leejuhanKr <96106122+leejuhanKr@users.noreply.github.com>
2022-10-03 21:49:17 +0900

    style: 세부 css 스타일 조정

--- client/src/components/modules/TabBar/TabBar.style.ts
+++ client/src/components/modules/TabBar/TabBar.style.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const TabBarLayout = styled.nav`
   display: flex;
-  justify-content: space-between;
+  justify-content: space-around;
   align-items: center;
   padding: 0px 25px;
   width: 300px;
@@ -10,9 +10,10 @@ export const TabBarLayout = styled.nav`
   box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.25);
   border-radius: 43px;
   position: absolute;
-  left: calc(50% - 230px / 2);
-  bottom: 250px;
+  left: calc(50% - 300px / 2);
+  bottom: 3.5%; // Iphone11 에서는 30px
 `
+
 export const IconBox = styled.div`
   display: flex;
   justify-content: center;
