d8956caac987aaf65834e6c9916ed08c7f77b629
ddaisylee <elliejay9988@gmail.com>
2022-10-06 11:11:38 +0900

    style: Comment 컴포넌트 ui 수정

--- client/src/components/atoms/Comment/Comment.tsx
+++ client/src/components/atoms/Comment/Comment.tsx
@@ -1,8 +1,8 @@
 import { FC } from 'react'
-
 import Avatar from '@/components/atoms/Avatar'
 import SvgButton from '@/components/atoms/SvgButton'
 import * as S from './Comment.style'
+import { AvatarCssProp, SvgButtonCssProp } from './Comment.style'
 
 interface Props {
   imageUrl: string
@@ -13,9 +13,16 @@ interface Props {
 const Comment: FC<Props> = ({ imageUrl, userName, children }) => {
   return (
     <S.Commentlayout>
-      <Avatar diameter="25px" imageUrl={imageUrl} name={userName} />
-      {children}
-      <SvgButton icon="HeartIcon" />
+      <S.AvatarBox>
+        <Avatar
+          diameter="25px"
+          imageUrl={imageUrl}
+          name={userName}
+          cssProp={AvatarCssProp}
+        />
+      </S.AvatarBox>
+      <S.ContentBox>{children}</S.ContentBox>
+      <SvgButton icon="HeartIcon" cssProp={SvgButtonCssProp} />
     </S.Commentlayout>
   )
 }
