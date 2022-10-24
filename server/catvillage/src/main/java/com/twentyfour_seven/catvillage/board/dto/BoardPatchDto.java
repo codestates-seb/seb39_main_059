9f9951b6cb5beae3efa0056fd5fa3eff291348af
WiseJade <jeonhr861@gmail.com>
2022-10-25 04:07:13 +0900

    fix: boardPatchDto에 유효성 검증 애너테이션 누락되어 추가

--- server/catvillage/src/main/java/com/twentyfour_seven/catvillage/board/dto/BoardPatchDto.java
+++ server/catvillage/src/main/java/com/twentyfour_seven/catvillage/board/dto/BoardPatchDto.java
@@ -6,20 +6,28 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@Validated
 public class BoardPatchDto {
     private Long boardId;
     @Length(max = 64)
     private String title;
     @Length(max = 1000)
     private String body;
+
+    @NotEmpty
     private List<BoardTagDto> tags = new ArrayList<>();
+
+    @Valid
     private List<PictureDto> pictures = new ArrayList<>();
 
     @Builder
