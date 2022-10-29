81d051bfa6862f821098592d71011899282b4f24
leejuhanKr <96106122+leejuhanKr@users.noreply.github.com>
2022-10-29 16:46:50 +0900

    feat: 집사생활 게시글 생성 페이지 ui 작성

--- client/src/pages/Board/NewBoard/NewBoard.tsx
+++ client/src/pages/Board/NewBoard/NewBoard.tsx
@@ -1,4 +1,23 @@
+import { axiosInstance } from '@Utils/instance'
+import axios from 'axios'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { useEffect } from 'react'
+import FeedForm from '@Template/FeedForm'
+import { useAppSelector } from '@/redux/store'
+
 const NewBoard = () => {
-  return <div>NewBoard</div>
+  const {pathname} = useLocation()
+  const navigate = useNavigate()
+  const { isLogin } = useAppSelector(state => state.user)
+
+  // login 컴포넌트의 useeffect로 인해 리다이렉션 적용안되는 중
+  useEffect(() => {
+    if (!isLogin) {
+      alert('로그인이 필요한 서비스입니다🐱')
+      navigate(`/login?redirectUrl=${pathname}`)
+    }
+  })
+
+  return (<FeedForm hasTitle/>)
 }
 export default NewBoard
