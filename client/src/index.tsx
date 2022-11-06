218047cd833cbe14ae10406c817cbb62eb04562f
ddaisylee <elliejay9988@gmail.com>
2022-11-06 17:07:46 +0900

    feat: redux-persist 설치 및 설정
    src/redux/store/index.js에서 redux-persist 설정
    
    src/index.js에서 App 컴포넌트에 적용

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { persistStore } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
 import App from './App'
+import { setupStore } from './redux/store'
+
+const store = setupStore()
+
+const persistor = persistStore(store)
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>,
 )
--- client/src/redux/store/index.ts
+++ client/src/redux/store/index.ts
@@ -4,10 +4,20 @@ import {
   PreloadedState,
 } from '@reduxjs/toolkit'
 import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux'
-import { userReducer } from '../reducers/userSlice'
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import thunk from 'redux-thunk'
+import { userReducer } from '../reducers/UserSlice'
 import { feedsReducer } from '../reducers/FeedsSlice'
 import { feedDetailReducer } from '../reducers/FeedDetailSlice'
 import { boardsReducer } from '../reducers/BoardsSlice'
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+  whitelist: ['user'],
+}
 
 const rootReducer = combineReducers({
   /* 슬라이스로 생성한 리듀서는 여기에 추가하면 됩니다. */
@@ -17,13 +27,19 @@ const rootReducer = combineReducers({
   boards: boardsReducer,
 })
 
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 export const setupStore = (preloadedState?: PreloadedState<RootState>) => {
   return configureStore({
-    reducer: rootReducer,
+    reducer: persistedReducer,
     preloadedState,
+    middleware: getDefaultMiddleware =>
+      getDefaultMiddleware({
+        serializableCheck: false,
+      }).concat(thunk),
+    devTools: process.env.NODE_ENV !== 'production',
   })
 }
-
 export const useAppDispatch: () => AppDispatch = useDispatch
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
 
