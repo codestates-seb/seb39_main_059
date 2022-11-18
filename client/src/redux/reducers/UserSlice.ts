cb216440b8bd613d82315a04c9afe7358067eecd
leejuhanKr <96106122+leejuhanKr@users.noreply.github.com>
2022-11-18 13:38:12 +0900

    feat: 로그아웃 로직 추가

--- client/src/redux/reducers/UserSlice.ts
+++ client/src/redux/reducers/UserSlice.ts
@@ -1,6 +1,6 @@
 import { createSlice, Reducer } from '@reduxjs/toolkit'
 import { UserInitialState } from '@Types/user'
-import { signupAsync, loginAsync } from '../actions/userAction'
+import { signupAsync, loginAsync, logoutAsync } from '../actions/userAction'
 
 const initialState: UserInitialState = {
   isLoading: true,
@@ -49,6 +49,28 @@ const userSlice = createSlice({
       .addCase(loginAsync.rejected, state => {
         state.isLoading = false
         alert('로그인 실패')
+      })
+      // 로그아웃
+      .addCase(logoutAsync.pending, state => {
+        state.isLogin = true
+      })
+      .addCase(logoutAsync.fulfilled, state => {
+        state.isLoading= true
+        state.isSignup= false
+        state.isLogin= false
+        state.userInfo= {
+          email: '',
+          password: '',
+          location: null,
+          name: '',
+          profileImage: null,
+          userId: '',
+        }
+        alert('로그아웃 성공!')
+      })
+      .addCase(logoutAsync.rejected, state => {
+        state.isLogin = true
+        alert('로그아웃 실패')
       })
   },
 })
