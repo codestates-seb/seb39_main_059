b3f55dd62d7453087f51630b3d0b1178a477a2e4
ddaisylee <elliejay9988@gmail.com>
2022-11-19 18:56:04 +0900

    feat: 드롭다운 메뉴 삭제하기 버튼에 onClick 핸들러 전달

--- client/src/components/modules/Dropdown/Dropdown.tsx
+++ client/src/components/modules/Dropdown/Dropdown.tsx
@@ -1,9 +1,16 @@
+import { FC } from 'react'
 import * as S from './Dropdown.styles'
 
-const Dropdown = () => {
+interface Props {
+  handleDeleteFeed: () => void
+}
+
+const Dropdown: FC<Props> = ({ handleDeleteFeed }) => {
   return (
     <S.Dropdown>
-      <li>삭제하기</li>
+      <li role="presentation" onClick={handleDeleteFeed}>
+        삭제하기
+      </li>
     </S.Dropdown>
   )
 }
