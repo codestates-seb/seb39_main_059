47c14af863c8c6ee461a0f3f87e66fcf46d4bc1a
ddaisylee <elliejay9988@gmail.com>
2022-11-20 23:27:26 +0900

    feat: 냥이생활 피드 작성 기능 구현

--- client/src/pages/Feed/NewFeed/NewFeed.tsx
+++ client/src/pages/Feed/NewFeed/NewFeed.tsx
@@ -1,30 +1,106 @@
 import PostForm from '@Template/PostForm'
-import { useEffect } from 'react'
+import { useEffect, useState, useCallback } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { SubmitHandler } from 'react-hook-form'
 import { FormValue } from '@Template/PostForm/PostForm'
 import Header from '@Modules/Header'
+import { axiosInstance } from '@Utils/instance'
+import { FEED_PATH } from '@Routes/feed.routes'
 import { useAppSelector } from '@/redux/store'
 import * as S from './NewFeed.style'
+
+interface Props {
+  catId: 0
+  name: string
+  profileImage: string
+}
 
 const NewFeed = () => {
   const navigate = useNavigate()
   const { isLogin } = useAppSelector(state => state.user)
+  const [catInfos, setCatInfos] = useState<Props[]>([])
+  const [catId, setCatId] = useState<number | null>(null)
+
+  const hasCatInfos = (data: any) => {
+    if (!data.length) {
+      if (
+        window.confirm(
+          '고양이를 등록하면 피드를 작성할 수 있습니다.\n등록 페이지로 이동하시겠습니까?',
+        )
+      ) {
+        navigate('/cat')
+      } else {
+        navigate('/')
+      }
+    }
+  }
+
+  const getCatInfos = useCallback(
+    async (callback: any) => {
+      const { data } = await axiosInstance.get('/users/cats', {
+        headers: {
+          contentType: 'application/json',
+          Authorization: `Bearer ${localStorage.getItem('ACCESS_TOKEN')}`,
+        },
+      })
+      setCatInfos(data)
+      callback(data)
+    },
+    [catInfos],
+  )
 
   useEffect(() => {
     if (!isLogin) {
       alert('로그인이 필요한 서비스입니다🐱')
       navigate('/login')
+      return
     }
-  }, [isLogin])
+    getCatInfos(hasCatInfos)
+  }, [])
 
   const onSubmitHandler: SubmitHandler<FormValue> = async data => {
-    // do something
+    const { picture, body } = data
+    const formData = {
+      body,
+      catId,
+      pictures: [
+        {
+          picture,
+        },
+      ],
+      tags: null,
+    }
+    const axiosResponse = await axiosInstance.post(FEED_PATH, formData, {
+      headers: {
+        contentType: 'application/json',
+        Authorization: `Bearer ${localStorage.getItem('ACCESS_TOKEN')}`,
+      },
+    })
+    navigate(-1)
+    return axiosResponse
   }
+
+  const catList: JSX.Element[] = catInfos.map((catInfo, index) => (
+    // eslint-disable-next-line react/no-array-index-key
+    <option key={index} value={catInfo.catId}>
+      {catInfo.name}
+    </option>
+  ))
 
   return (
     <S.NewFeedLayout>
       <Header>냥이생활 글쓰기</Header>
+      <div>
+        <select
+          onChange={e => {
+            console.log(e.target.value)
+            setCatId(Number(e.target.value))
+          }}
+        >
+          <option value={0}>--고양이 선택--</option>
+          {catList && catList}
+        </select>
+      </div>
       <PostForm onSubmitHandler={onSubmitHandler} />
     </S.NewFeedLayout>
   )
