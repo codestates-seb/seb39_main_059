2b6635fc8eafdd73ba4dbf04763110eb8c177197
leejuhanKr <96106122+leejuhanKr@users.noreply.github.com>
2022-11-25 16:42:38 +0900

    refactor: 토큰 재발급시 로컬스트로지의 토큰도 갱신하도록 변경

--- client/src/api/auth.ts
+++ client/src/api/auth.ts
@@ -1,14 +1,21 @@
 import { axiosInstance } from '@Utils/instance'
+import { setUserToken } from '@Utils/tokenControl'
 
 export const renewAccessToken = async (refreshToken: string) => {
   const { data } = await axiosInstance.post('/reissue', {
     refreshToken,
   })
-  const { accessToken } = data
+  const { accessToken: renewedAccessToken, refreshToken: renewedRefreshToken } =
+    data
 
-  if (!accessToken) {
+  if (!renewedAccessToken || !renewedRefreshToken) {
     throw new Error('토큰을 받지 못했습니다.')
   }
+  setUserToken('ACCESS_TOKEN', renewedAccessToken)
+  setUserToken('REFRESH_TOKEN', renewedRefreshToken)
 
-  return accessToken as string
+  return { renewedAccessToken, renewedRefreshToken } as {
+    renewedAccessToken: string
+    renewedRefreshToken: string
+  }
 }
